42b931d1046fa5ba4cd76fa979e28c0dad77d545
tansquire <iitmadraselectrical@gmail.com>
2017-12-05 01:21:41 -0800

    added switch case

---
+++ src/core/SwitchCase_Number.java
@@ -0,0 +1,28 @@
+
+package core;
+
+
+public class SwitchCase_Number 
+{
+    public static void main(String[] args) 
+    {
+        int i=9;
+        switch(i)
+        {
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("Nothing");
+                
+        }
+        
+    }
+    
+}
---
+++ src/core/Switch_Case_String.java
@@ -0,0 +1,24 @@
+public class Switch_Case_String 
+{
+    public static void main(String[] args) 
+    {
+        String i="hi";
+        switch(i)
+        {
+            case "hi":
+                System.out.println("hi");
+                break;
+            case "hello":
+                System.out.println("hello");
+                break;
+            case "what":
+                System.out.println("what");
+                break;
+            default:
+                System.out.println("Nothing");
+                
+        }
+        
+    }
+    
+}
