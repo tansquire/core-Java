93f410a367306d4c93a215fb9adf049dead919eb
tansquire <iitmadraselectrical@gmail.com>
2017-12-05 16:18:51 -0800

    added loop

---
+++ src/core/Loop.java
@@ -0,0 +1,29 @@
+
+package core;
+
+
+public class Loop 
+{
+    public static void main(String[] args)
+    {
+        int i,j,k;
+        for(i=0;i<10;i++)
+        System.out.println(i);
+        
+      j=0;
+      while(j<10)
+      {
+          System.out.println(j);
+          j++;
+      }
+      
+      k=0;
+      do
+      {
+          System.out.println(k);
+          k++;
+      }while(k<10);
+        
+    }
+    
+}
